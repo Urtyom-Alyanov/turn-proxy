72497d2da586c4b04bc31c89f60dc8167b37a916
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-03-18 23:03:21 +0300

    chore: increase version

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
 
         turn-proxy-pkg = pkgs.rustPlatform.buildRustPackage {
           pname = "turn-proxy-server";
-          version = "1.0.6";
+          version = "1.0.7";
           src = ./.;
 
           cargoLock = {
