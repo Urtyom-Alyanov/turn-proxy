3c86d5d7ae6f83a551c527730431e946ddfdf791
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-04-07 18:58:46 +0300

    fix: idle timer

--- lib/src/proxy/flow.rs
+++ lib/src/proxy/flow.rs
@@ -1,9 +1,9 @@
 use std::{net::SocketAddr, sync::Arc, time::Duration};
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use tokio::{sync::RwLock, task::JoinHandle};
 use tokio_util::sync::CancellationToken;
-use tracing::{debug, warn};
+use tracing::{debug, info, warn};
 use webrtc_util::Conn;
 
 /// Низкоуровневая абстракция
@@ -27,62 +27,75 @@ pub fn proxy_flow(
     let mut buf = [0u8; 2048];
 
     loop {
-      if cancellation_token.is_cancelled() {
+      let recv_future = from_flow.recv_from(&mut buf);
+
+      let res = tokio::select! {
+        // Отмена потока
+        _ = cancellation_token.cancelled() => break,
+
+        // Получение данных с возможным таймаутом бездействия
+        recv_result = async {
+          if let Some(t) = idle_timeout {
+            tokio::time::timeout(t, recv_future).await.map_err(|_| anyhow!("Idle timeout reached"))
+          } else {
+            Ok(recv_future.await)
+          }
+        } => {
+          match recv_result {
+            Ok(Ok(res)) => res,
+            Ok(Err(e)) => {
+              warn!("[{}] Receive error: {}", flow_name, e);
+              break;
+            }
+            Err(_) => {
+              debug!("[{}] Idle timeout reached", flow_name);
+              break;
+            }
+          }
+        }
+      };
+
+      let (n, src) = res;
+
+      if n == 0 {
         break;
       }
 
-      let recv_result = if let Some(t) = idle_timeout {
-        match tokio::time::timeout(t, from_flow.recv_from(&mut buf)).await {
-          Ok(res) => res,
-          Err(_) => {
-            debug!("[{}] Idle timeout reached ({}s)", flow_name, t.as_secs());
-            break;
-          }
-        }
+      if let Some(cache) = &from_cache {
+        *cache.write().await = Some(src);
+      }
+
+      debug!("[{}] Received {} bytes from {}", flow_name, n, src);
+
+      if n >= buf.len() {
+        warn!(
+          "[{}] Packet from {} is too large for buffer ({})",
+          flow_name, src, n
+        );
+      }
+
+      let (send, dest) = if let Some(cache) = &to_cache {
+        let dest = cache.read().await.unwrap_or(to_addr);
+        (to_flow.send_to(&buf[..n], dest), dest)
       } else {
-        from_flow.recv_from(&mut buf).await
+        (to_flow.send(&buf[..n]), to_addr)
       };
 
-      match recv_result {
-        Ok((n, src)) if n > 0 => {
-          if let Some(cache) = &from_cache {
-            *cache.write().await = Some(src);
-          }
-
-          debug!("[{}] Received {} bytes from {}", flow_name, n, src);
-          if n >= buf.len() {
-            warn!(
-              "[{}] Packet from {} is too large for buffer ({})",
-              flow_name, src, n
-            );
-          }
-          if let Some(cache) = &to_cache {
-            let dest = cache.read().await.unwrap_or(to_addr);
-            if let Err(e) = to_flow.send_to(&buf[..n], dest).await {
-              warn!(
-                "[{}] Error sending to {} from {}: {}",
-                flow_name, dest, src, e
-              );
-              break;
-            }
-            debug!("[{}] Send {} bytes into {}", flow_name, n, dest);
-          } else {
-            if let Err(e) = to_flow.send(&buf[..n]).await {
-              warn!(
-                "[{}] Error sending to {} from {}: {}",
-                flow_name, to_addr, src, e
-              );
-              break;
-            }
-            debug!("[{}] Send {} bytes into {}", flow_name, n, to_addr);
-          }
+      let sent_bytes = match send.await {
+        Ok(n) => n,
+        Err(e) => {
+          warn!("[{}] Send error to {}: {}", flow_name, dest, e);
+          break;
         }
-        _ => break,
-      }
+      };
+
+
+      debug!("[{}] Sent {} bytes to {}", flow_name, sent_bytes, dest);
+      
     }
 
     cancellation_token.cancel();
-    debug!("[{}] Flow stopped", flow_name);
+    info!("[{}] Flow stopped", flow_name);
     Ok(())
   })
 }
